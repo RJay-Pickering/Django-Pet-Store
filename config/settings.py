001725a1542739de42389de0fad854b657a7e2e3
RJay-Pickering <rjaypickering@gmail.com>
2024-01-03 16:14:50 -0600

    getting it set up on render

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = "django-insecure-f49p%bh7dd%fhn1)-a_$=tg9+)v468zfm9!7sg3efwz5)b_7k2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: List = []
+ALLOWED_HOSTS: List = ["apawcalyptic-partners.onrender.com"]
 
 
 # Application definition
@@ -120,8 +120,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "app/static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
