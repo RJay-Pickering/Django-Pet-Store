36316d7bcac721fe0be2bab028f69a69ec128753
RJay-Pickering <rjaypickering@gmail.com>
2024-01-04 11:04:39 -0600

    added some vercel configurations that will hopefully work out

--- config/settings.py
+++ config/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS: List = [
     "apawcalyptic-partners.onrender.com",
-    "apawcalyptic-partners.vercel.app",
+    ".vercel.app",
     "localhost",
     "127.0.0.1",
 ]
