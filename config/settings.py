0ad8f5820753518f7a384b2fd7f10cc3f302df0e
RJay-Pickering <rjaypickering@gmail.com>
2024-01-04 10:34:39 -0600

    testing another deployable site?

--- config/settings.py
+++ config/settings.py
@@ -27,7 +27,12 @@ SECRET_KEY = "django-insecure-f49p%bh7dd%fhn1)-a_$=tg9+)v468zfm9!7sg3efwz5)b_7k2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: List = ["apawcalyptic-partners.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS: List = [
+    "apawcalyptic-partners.onrender.com",
+    "apawcalyptic-partners.vercel.app",
+    "localhost",
+    "127.0.0.1",
+]
 
 
 # Application definition
